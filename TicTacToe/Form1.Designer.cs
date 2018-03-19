281688a5fe2095c1aab6817a4865de2ab08ad048
NostradamusWii <niazabsaliamov@gmail.com>
2018-03-19 16:08:45 +0300

    Added New game button and controls for keys

--- TicTacToe/Form1.Designer.cs
+++ TicTacToe/Form1.Designer.cs
@@ -38,6 +38,7 @@
             this.Key6 = new System.Windows.Forms.Button();
             this.Key8 = new System.Windows.Forms.Button();
             this.Key9 = new System.Windows.Forms.Button();
+            this.newGameButton = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -59,81 +60,111 @@
             // 
             // Key1
             // 
+            this.Key1.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Key1.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Key1.Location = new System.Drawing.Point(3, 3);
             this.Key1.Name = "Key1";
             this.Key1.Size = new System.Drawing.Size(96, 96);
             this.Key1.TabIndex = 1;
             this.Key1.UseVisualStyleBackColor = true;
+            this.Key1.Click += new System.EventHandler(this.Key1_Click);
             // 
             // Key2
             // 
+            this.Key2.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Key2.Location = new System.Drawing.Point(102, 3);
             this.Key2.Name = "Key2";
             this.Key2.Size = new System.Drawing.Size(96, 96);
             this.Key2.TabIndex = 1;
             this.Key2.UseVisualStyleBackColor = true;
+            this.Key2.Click += new System.EventHandler(this.Key2_Click);
             // 
             // Key3
             // 
+            this.Key3.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Key3.Location = new System.Drawing.Point(201, 3);
             this.Key3.Name = "Key3";
             this.Key3.Size = new System.Drawing.Size(96, 96);
             this.Key3.TabIndex = 1;
             this.Key3.UseVisualStyleBackColor = true;
+            this.Key3.Click += new System.EventHandler(this.Key3_Click);
             // 
             // Key4
             // 
+            this.Key4.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Key4.Location = new System.Drawing.Point(3, 102);
             this.Key4.Name = "Key4";
             this.Key4.Size = new System.Drawing.Size(96, 96);
             this.Key4.TabIndex = 1;
             this.Key4.UseVisualStyleBackColor = true;
+            this.Key4.Click += new System.EventHandler(this.Key4_Click);
             // 
             // Key7
             // 
+            this.Key7.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Key7.Location = new System.Drawing.Point(3, 201);
             this.Key7.Name = "Key7";
             this.Key7.Size = new System.Drawing.Size(96, 96);
             this.Key7.TabIndex = 1;
             this.Key7.UseVisualStyleBackColor = true;
+            this.Key7.Click += new System.EventHandler(this.Key7_Click);
             // 
             // Key5
             // 
+            this.Key5.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Key5.Location = new System.Drawing.Point(102, 102);
             this.Key5.Name = "Key5";
             this.Key5.Size = new System.Drawing.Size(96, 96);
             this.Key5.TabIndex = 1;
             this.Key5.UseVisualStyleBackColor = true;
+            this.Key5.Click += new System.EventHandler(this.Key5_Click);
             // 
             // Key6
             // 
+            this.Key6.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Key6.Location = new System.Drawing.Point(201, 102);
             this.Key6.Name = "Key6";
             this.Key6.Size = new System.Drawing.Size(96, 96);
             this.Key6.TabIndex = 1;
             this.Key6.UseVisualStyleBackColor = true;
+            this.Key6.Click += new System.EventHandler(this.Key6_Click);
             // 
             // Key8
             // 
+            this.Key8.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Key8.Location = new System.Drawing.Point(102, 201);
             this.Key8.Name = "Key8";
             this.Key8.Size = new System.Drawing.Size(96, 96);
             this.Key8.TabIndex = 1;
             this.Key8.UseVisualStyleBackColor = true;
+            this.Key8.Click += new System.EventHandler(this.Key8_Click);
             // 
             // Key9
             // 
+            this.Key9.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Key9.Location = new System.Drawing.Point(201, 201);
             this.Key9.Name = "Key9";
             this.Key9.Size = new System.Drawing.Size(96, 96);
             this.Key9.TabIndex = 1;
             this.Key9.UseVisualStyleBackColor = true;
+            this.Key9.Click += new System.EventHandler(this.Key9_Click);
+            // 
+            // newGameButton
+            // 
+            this.newGameButton.Location = new System.Drawing.Point(315, 15);
+            this.newGameButton.Name = "newGameButton";
+            this.newGameButton.Size = new System.Drawing.Size(106, 45);
+            this.newGameButton.TabIndex = 2;
+            this.newGameButton.Text = "New game";
+            this.newGameButton.UseVisualStyleBackColor = true;
+            this.newGameButton.Click += new System.EventHandler(this.newGameButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(529, 368);
+            this.Controls.Add(this.newGameButton);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -154,6 +185,7 @@
         private System.Windows.Forms.Button Key7;
         private System.Windows.Forms.Button Key4;
         private System.Windows.Forms.Button Key1;
+        private System.Windows.Forms.Button newGameButton;
     }
 }
 
--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -12,9 +12,71 @@ namespace TicTacToe
 {
     public partial class Form1 : Form
     {
+        private string Side
+        {
+            get { return _isX ? "X" : "O"; }
+        }
+
+        private bool _isX;
         public Form1()
         {
             InitializeComponent();
+            _isX = MessageBox.Show(@"Playing for X?", @"Playing for X?", MessageBoxButtons.YesNo) == DialogResult.Yes;
+        }
+
+        private void Key1_Click(object sender, EventArgs e)
+        {
+            Key1.Text = Side;
+        }
+
+        private void Key2_Click(object sender, EventArgs e)
+        {
+            Key2.Text = Side;
+        }
+
+        private void Key3_Click(object sender, EventArgs e)
+        {
+            Key3.Text = Side;
+        }
+
+        private void Key4_Click(object sender, EventArgs e)
+        {
+            Key4.Text = Side;
+        }
+
+        private void Key5_Click(object sender, EventArgs e)
+        {
+            Key5.Text = Side;
+        }
+
+        private void Key6_Click(object sender, EventArgs e)
+        {
+            Key6.Text = Side;
+        }
+
+        private void Key7_Click(object sender, EventArgs e)
+        {
+            Key7.Text = Side;
+        }
+
+        private void Key8_Click(object sender, EventArgs e)
+        {
+            Key8.Text = Side;
+        }
+
+        private void Key9_Click(object sender, EventArgs e)
+        {
+            Key9.Text = Side;
+        }
+
+        private void newGameButton_Click(object sender, EventArgs e)
+        {
+            var keys = new[] { Key1, Key2, Key3, Key4, Key5, Key6, Key7, Key8, Key9 };
+            foreach (var key in keys)
+            {
+                key.Text = "";
+            }
+            _isX = MessageBox.Show(@"Playing for X?", @"Playing for X?", MessageBoxButtons.YesNo) == DialogResult.Yes;
         }
     }
 }
