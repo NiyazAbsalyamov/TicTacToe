3296a36e1b85c2e6ca8f14f172c720b775e28f1a
NostradamusWii <niazabsaliamov@gmail.com>
2018-03-19 15:30:57 +0300

    Added xo buttons

--- TicTacToe/Form1.Designer.cs
+++ TicTacToe/Form1.Designer.cs
@@ -28,12 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Key1 = new System.Windows.Forms.Button();
+            this.Key2 = new System.Windows.Forms.Button();
+            this.Key3 = new System.Windows.Forms.Button();
+            this.Key4 = new System.Windows.Forms.Button();
+            this.Key7 = new System.Windows.Forms.Button();
+            this.Key5 = new System.Windows.Forms.Button();
+            this.Key6 = new System.Windows.Forms.Button();
+            this.Key8 = new System.Windows.Forms.Button();
+            this.Key9 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.Key3);
+            this.panel1.Controls.Add(this.Key9);
+            this.panel1.Controls.Add(this.Key8);
+            this.panel1.Controls.Add(this.Key6);
+            this.panel1.Controls.Add(this.Key5);
+            this.panel1.Controls.Add(this.Key2);
+            this.panel1.Controls.Add(this.Key7);
+            this.panel1.Controls.Add(this.Key4);
+            this.panel1.Controls.Add(this.Key1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 300);
+            this.panel1.TabIndex = 0;
+            // 
+            // Key1
+            // 
+            this.Key1.Location = new System.Drawing.Point(3, 3);
+            this.Key1.Name = "Key1";
+            this.Key1.Size = new System.Drawing.Size(96, 96);
+            this.Key1.TabIndex = 1;
+            this.Key1.UseVisualStyleBackColor = true;
+            // 
+            // Key2
+            // 
+            this.Key2.Location = new System.Drawing.Point(102, 3);
+            this.Key2.Name = "Key2";
+            this.Key2.Size = new System.Drawing.Size(96, 96);
+            this.Key2.TabIndex = 1;
+            this.Key2.UseVisualStyleBackColor = true;
+            // 
+            // Key3
+            // 
+            this.Key3.Location = new System.Drawing.Point(201, 3);
+            this.Key3.Name = "Key3";
+            this.Key3.Size = new System.Drawing.Size(96, 96);
+            this.Key3.TabIndex = 1;
+            this.Key3.UseVisualStyleBackColor = true;
+            // 
+            // Key4
+            // 
+            this.Key4.Location = new System.Drawing.Point(3, 102);
+            this.Key4.Name = "Key4";
+            this.Key4.Size = new System.Drawing.Size(96, 96);
+            this.Key4.TabIndex = 1;
+            this.Key4.UseVisualStyleBackColor = true;
+            // 
+            // Key7
+            // 
+            this.Key7.Location = new System.Drawing.Point(3, 201);
+            this.Key7.Name = "Key7";
+            this.Key7.Size = new System.Drawing.Size(96, 96);
+            this.Key7.TabIndex = 1;
+            this.Key7.UseVisualStyleBackColor = true;
+            // 
+            // Key5
+            // 
+            this.Key5.Location = new System.Drawing.Point(102, 102);
+            this.Key5.Name = "Key5";
+            this.Key5.Size = new System.Drawing.Size(96, 96);
+            this.Key5.TabIndex = 1;
+            this.Key5.UseVisualStyleBackColor = true;
+            // 
+            // Key6
+            // 
+            this.Key6.Location = new System.Drawing.Point(201, 102);
+            this.Key6.Name = "Key6";
+            this.Key6.Size = new System.Drawing.Size(96, 96);
+            this.Key6.TabIndex = 1;
+            this.Key6.UseVisualStyleBackColor = true;
+            // 
+            // Key8
+            // 
+            this.Key8.Location = new System.Drawing.Point(102, 201);
+            this.Key8.Name = "Key8";
+            this.Key8.Size = new System.Drawing.Size(96, 96);
+            this.Key8.TabIndex = 1;
+            this.Key8.UseVisualStyleBackColor = true;
+            // 
+            // Key9
+            // 
+            this.Key9.Location = new System.Drawing.Point(201, 201);
+            this.Key9.Name = "Key9";
+            this.Key9.Size = new System.Drawing.Size(96, 96);
+            this.Key9.TabIndex = 1;
+            this.Key9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 368);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Key3;
+        private System.Windows.Forms.Button Key9;
+        private System.Windows.Forms.Button Key8;
+        private System.Windows.Forms.Button Key6;
+        private System.Windows.Forms.Button Key5;
+        private System.Windows.Forms.Button Key2;
+        private System.Windows.Forms.Button Key7;
+        private System.Windows.Forms.Button Key4;
+        private System.Windows.Forms.Button Key1;
     }
 }
 
