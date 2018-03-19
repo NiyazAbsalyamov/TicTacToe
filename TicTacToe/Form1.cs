c70bd21d6378599b82aeee022673ae91f50f6e30
NostradamusWii <niazabsaliamov@gmail.com>
2018-03-19 18:48:51 +0300

    Removed usings and added model methods

--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using TicTacToe.Models;
 
@@ -17,7 +10,7 @@ namespace TicTacToe
 
         private bool _isX;
 
-        private Model _model;
+        private readonly Model _model;
 
         public Form1()
         {
@@ -29,46 +22,55 @@ namespace TicTacToe
         private void Key1_Click(object sender, EventArgs e)
         {
             Key1.Text = Side;
+            _model.Set(1, true, _isX, false);
         }
 
         private void Key2_Click(object sender, EventArgs e)
         {
             Key2.Text = Side;
+            _model.Set(2, true, _isX, false);
         }
 
         private void Key3_Click(object sender, EventArgs e)
         {
             Key3.Text = Side;
+            _model.Set(3, true, _isX, false);
         }
 
         private void Key4_Click(object sender, EventArgs e)
         {
             Key4.Text = Side;
+            _model.Set(4, true, _isX, false);
         }
 
         private void Key5_Click(object sender, EventArgs e)
         {
             Key5.Text = Side;
+            _model.Set(5, true, _isX, false);
         }
 
         private void Key6_Click(object sender, EventArgs e)
         {
             Key6.Text = Side;
+            _model.Set(6, true, _isX, false);
         }
 
         private void Key7_Click(object sender, EventArgs e)
         {
             Key7.Text = Side;
+            _model.Set(7, true, _isX, false);
         }
 
         private void Key8_Click(object sender, EventArgs e)
         {
             Key8.Text = Side;
+            _model.Set(8, true, _isX, false);
         }
 
         private void Key9_Click(object sender, EventArgs e)
         {
             Key9.Text = Side;
+            _model.Set(9, true, _isX, false);
         }
 
         private void newGameButton_Click(object sender, EventArgs e)
@@ -78,6 +80,7 @@ namespace TicTacToe
             {
                 key.Text = "";
             }
+            _model.Reset();
             _isX = MessageBox.Show(@"Playing for X?", @"Playing for X?", MessageBoxButtons.YesNo) == DialogResult.Yes;
         }
     }
--- TicTacToe/Models/Model.cs
+++ TicTacToe/Models/Model.cs
@@ -1,18 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace TicTacToe.Models
 {
     public class Model
     {
-        public Dictionary<int, Cell> Field;
+        private Dictionary<int, Cell> _field;
 
         public Model()
         {
-            Field = new Dictionary<int, Cell>();
+            _field = new Dictionary<int, Cell>();
+
+            for (int i = 1; i < 10; i++)
+            {
+                _field.Add(i, new Cell());
+            }
+        }
+
+        public void Set(int cell, bool isPlayer, bool isX, bool isEmpty)
+        {
+            if (isEmpty)
+            {
+                _field[cell].Reset();
+                return;
+            }
+
+            _field[cell].Set(isPlayer, isX);
+        }
+
+        public void Reset()
+        {
+            foreach (var cell in _field)
+            {
+                cell.Value.Reset();
+            }
         }
     }
 }
